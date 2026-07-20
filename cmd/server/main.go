60d37e4d744dffe58d3202d0cb47ba847a355fd1
Amarendra Behera <amarendra.behera@oracle.com>
2026-07-20 22:35:55 +0530

    First server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	const port = "8080"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Ok!")
+	})
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
