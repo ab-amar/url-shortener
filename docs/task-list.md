d55387a66ba010b57242859f7a3421a6f6f69c99
Amarendra Behera <ab.amar2112@gmail.com>
2026-07-28 15:09:50 +0530

    Add postgres repository

--- docs/task-list.md
+++ docs/task-list.md
@@ -53,10 +53,10 @@
 51. Design the first database schema for shortened URLs
 52. Discuss primary keys, unique constraints, and indexes
 53. Add SQL migrations strategy
+54. Add a PostgreSQL repository implementation
 
 ................................................
 
-54. Add a PostgreSQL repository implementation
 55. Compare in-memory vs PostgreSQL repository behavior
 56. Handle duplicate short code conflicts safely
 57. Add created-at and updated-at fields
---
+++ internal/repository/postgres_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/ab-amar/url-shortener/internal/model"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresRepository struct {
+	Pool *pgxpool.Pool
+}
+
+func NewPostgresRepository(pool *pgxpool.Pool) PostgresRepository {
+	return PostgresRepository{
+		Pool: pool,
+	}
+}
+
+func (r *PostgresRepository) SaveURL(url model.URL) {
+	query := "INSERT INTO urls (short_code, original_url, created_at) VALUES ($1, $2, $3);"
+	r.Pool.Exec(context.Background(), query, url.ShortCode, url.OriginalURL, url.CreatedAt)
+}
+
+func (r *PostgresRepository) FindByCode(code string) (model.URL, bool) {
+	query := "SELECT original_url, short_code, created_at FROM urls WHERE short_code = $1;"
+	row := r.Pool.QueryRow(context.Background(), query, code)
+	var url model.URL
+	err := row.Scan(
+		&url.OriginalURL,
+		&url.ShortCode,
+		&url.CreatedAt,
+	)
+
+	if err == pgx.ErrNoRows {
+		return model.URL{}, false
+	}
+	if err != nil {
+		return model.URL{}, false
+	}
+	return url, true
+}
